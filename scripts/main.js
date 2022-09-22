20a6438217a7a390db155b661b742099c226e23d
TsujiAmisa <81774285+TsujiAmisa@users.noreply.github.com>
2022-09-22 11:08:20 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -7,11 +7,11 @@ let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/firefox-icon.png') {
+  //if(mySrc === 'images/firefox-icon.png') {
     myImage.setAttribute ('src','images/ana.jp2');
-  } else {
-    myImage.setAttribute ('src','images/firefox-icon.png');
-  }
+  //} else {
+  //  myImage.setAttribute ('src','images/firefox-icon.png');
+  //}
 }
 
 // Personalized welcome message code
